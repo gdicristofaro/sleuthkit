a71d6ceaca96557d9b9f9b0176ef41587a2df07a
Greg DiCristofaro <gregd@basistech.com>
2024-12-11 18:29:44 -0500

    commenting

--- test/tsk/hashdb/test_binsrch_index.cpp
+++ test/tsk/hashdb/test_binsrch_index.cpp
@@ -12,8 +12,14 @@ void test_hdb_binsrch_idx_init_hash_type_info(
 {
     TSK_HDB_BINSRCH_INFO hdb_binsrch_info;
 
+    /**
+     * setup values:
+     * normally, this would be done as a part of opening the hashdb, but not 
+     * for this unit test
+     **/
     hdb_binsrch_info.hash_type = TSK_HDB_HTYPE_INVALID_ID;
-    hdb_binsrch_info.base.db_fname = (TSK_TCHAR*) db_name;
+    // this value is read only in hdb_binsrch_idx_init_hash_type_info
+    hdb_binsrch_info.base.db_fname = (TSK_TCHAR *)db_name;
     int ret_val = hdb_binsrch_idx_init_hash_type_info(&hdb_binsrch_info, htype);
 
     REQUIRE(ret_val == expected_return);
@@ -26,12 +32,19 @@ void test_hdb_binsrch_idx_init_hash_type_info(
         REQUIRE(TSTRCMP(hdb_binsrch_info.idx_idx_fname, expected_idx_idx_fname) == 0);
     }
 
-    // cleanup allocated items from hdb_binsrch_idx_init_hash_type_info
-    if (hdb_binsrch_info.idx_fname != NULL) {
+    /**
+     * cleanup allocated items from hdb_binsrch_idx_init_hash_type_info.
+     * These are values set by hdb_binsrch_idx_init_hash_type_info during run 
+     * that need to be cleaned up. Normally, they would be cleaned up when the
+     * hash database is closed, but not for this unit test.
+     **/
+    if (hdb_binsrch_info.idx_fname != NULL)
+    {
         free(hdb_binsrch_info.idx_fname);
     }
 
-    if (hdb_binsrch_info.idx_idx_fname != NULL) {
+    if (hdb_binsrch_info.idx_idx_fname != NULL)
+    {
         free(hdb_binsrch_info.idx_idx_fname);
     }
 }
